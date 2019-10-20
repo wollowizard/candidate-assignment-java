ac3005872ff4b16c1d0b9f0dddc8a7c525de3c7e
Robert Blust <rob@3ap.ch>
2019-10-20 19:57:12 +0200

    fixed packing name

--- src/test/java/ch/aaap/assignment/ApplicationTest.java
+++ src/test/java/ch/aaap/assignment/ApplicationTest.java
@@ -1,4 +1,4 @@
-package ch.aaap.assignments;
+package ch.aaap.assignment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
